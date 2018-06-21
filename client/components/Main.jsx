2938c83bfa6898891f720c6a93378f6213a210b7
engie15 <e.matene15@gmail.com>
2018-06-21 13:50:17 +1200

    bulma module now connected to website'

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -8,7 +8,7 @@ class Main extends React.Component {
 
     render() {
         return (<div>
-                <h1>This is the Main component</h1>
+                <h1 className="title has-text-info">This is the Main component</h1>
                 <Me />
                 <Projects />
                 <You />
--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
+import './sass/main.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
